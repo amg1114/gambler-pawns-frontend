583dac860ccc132a729e9a647c614146d3a647eb
Alejandro Moreno <alejandromogil@gmail.com>
2024-08-31 20:42:09 -0500

    chore: update colors in tailwind.config.ts and add font imports in fonts.ts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Alfa_Slab_One, Bungee, Andika } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const alfaSlabOne = Alfa_Slab_One({ subsets: ["latin"], weight: ["400"] });
+export const bungee = Bungee({ subsets: ["latin"], weight: ["400"] });
+export const andika = Andika({ subsets: ["latin"], weight: ["400"] });
