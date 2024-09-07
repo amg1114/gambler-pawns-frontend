755ae76dadfee5f07e0ef3f1263055fcb66fbd29
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-07 15:34:01 -0500

    refactor: Update color names and add animations in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,12 +17,39 @@ const config: Config = {
       "success":"#3AFF13",
       "warning":"#FFFFFF",
       "light":"#EAE9FC",
+      "gray": "#333333"
     },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-out-up": {
+          "0%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-out-up": "fade-out-up 0.5s ease-out",
       },
     },
   },
