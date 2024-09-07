6ce72e8eeafc02a2df168b4249d21f721aae9914
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-07 14:15:34 -0500

    feat: Add extraClasses prop to StyledButton component

--- app/ui/components/StyledButton.tsx
+++ app/ui/components/StyledButton.tsx
@@ -1,29 +1,43 @@
 import { nunito } from "@/app/ui/fonts";
 
+/**
+ * StyledButton component
+ * @param {React.ReactNode} children - The content of the button
+ * @param {"primary" | "secondary"} variant - The variant of the button
+ * @param {"filled" | "outlined"} style - The style of the button
+ * @param {string} extraClasses - Extra classes to be added to the button
+ * @param {(event: React.MouseEvent<HTMLButtonElement>) => void} onClick - The click event handler
+ * @returns {JSX.Element} The styled button component
+ */
 interface StyledButtonProps {
     children: React.ReactNode;
     variant?: "primary" | "secondary";
     style?: "filled" | "outlined";
+    extraClasses?: string;
     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const styleClasses = {
     filled: {
-        primary: "bg-primary text-secondary hover:bg-secondary hover:text-primary",
-        secondary: "bg-secondary text-white hover:bg-primary hover:text-secondary",
+        primary:
+            "bg-primary text-secondary hover:bg-secondary hover:text-primary",
+        secondary:
+            "bg-secondary text-white hover:bg-primary hover:text-secondary",
     },
     outlined: {
-        primary: "border border-2 border-primary text-primary hover:bg-primary hover:text-secondary",
-        secondary: "border border-2 border-secondary text-secondary hover:bg-secondary hover:text-primary",
+        primary:
+            "border border-2 border-primary text-primary hover:bg-primary hover:text-secondary",
+        secondary:
+            "border border-2 border-secondary text-secondary hover:bg-secondary hover:text-primary",
     },
 };
 
 export default function StyledButton(props: StyledButtonProps) {
-    const { children } = props;
+    const { children, extraClasses, onClick } = props;
     return (
         <button
-            className={`${styleClasses[props.style || "filled"][ props.variant || "primary"]} w-fit shrink-0 px-4 py-2 rounded ${nunito.className} font-bold cursor-pointer transition-colors` 
-            }
+            className={`${styleClasses[props.style || "filled"][props.variant || "primary"]} w-fit shrink-0 rounded px-4 py-2 ${nunito.className} cursor-pointer font-bold transition-colors ${extraClasses}`}
+            onClick={onClick}
         >
             {children}
         </button>
