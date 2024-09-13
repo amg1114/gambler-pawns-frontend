9f444642de2e12d20aefa6b17f4c1631320cd9ef
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-09-13 14:56:12 -0500

    Pagina Base para GameOptions

---
+++ app/GameOptions/Classic/layout.tsx
@@ -0,0 +1,11 @@
+export default function ClassicOptionPage({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <main className="mx-auto w-full px-md md:w-4/5 md:max-w-screen-sm">
+            {children}
+        </main>
+    );
+}
