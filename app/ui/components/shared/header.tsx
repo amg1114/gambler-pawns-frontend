6001462c1067bf028a28089f0ecd42c54a719d1a
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-09-13 10:36:45 -0500

    Añadido renderizado condicional al header

--- app/ui/components/shared/header.tsx
+++ app/ui/components/shared/header.tsx
@@ -3,6 +3,34 @@ import Logo from "../../icons/logo.svg";
 import Coin from "../../icons/coin.svg";
 import Fire from "../../icons/fire.svg";
 
+function conditionalRendering() {
+    let userlogin = true;
+    if (userlogin) {
+        return (
+            <div className="flex w-1/3 justify-end max-[340px]:sr-only">
+                <text className="__className_94ea24 pr-xs text-base text-light">
+                    200
+                </text>
+                <Image
+                    src={Coin}
+                    alt=""
+                    width={20}
+                    height={20}
+                    className="h-auto w-auto"
+                />
+                <text className="__className_94ea24 pl-sm pr-xs text-base text-light">
+                    14
+                </text>
+                <Image src={Fire} alt="" width={14.78} height={20} />
+            </div>
+        );
+    } else {
+        return (
+            <div className="flex w-1/3 justify-end max-[340px]:sr-only"></div>
+        );
+    }
+}
+
 export default function Header() {
     return (
         <nav className="fixed flex w-full items-center justify-between border-b-2 border-primary bg-dark-1 p-md">
@@ -18,22 +46,7 @@ export default function Header() {
                 className="h-auto w-auto max-[200px]:sr-only"
             />
 
-            <div className="flex w-1/3 justify-end max-[340px]:sr-only">
-                <text className="__className_94ea24 pr-xs text-base text-light">
-                    200
-                </text>
-                <Image
-                    src={Coin}
-                    alt=""
-                    width={20}
-                    height={20}
-                    className="h-auto w-auto"
-                />
-                <text className="__className_94ea24 pr-xs pl-sm text-base text-light">
-                    14
-                </text>
-                <Image src={Fire} alt="" width={14.78} height={20} />
-            </div>
+            {conditionalRendering()}
         </nav>
     );
 }
