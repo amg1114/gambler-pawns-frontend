5ef08bd49e2887e51f1c4afb497ecde0abca3158
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-15 16:39:32 -0500

    feat: add dynamic data to user Profile Page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 // Libs
 import "/node_modules/flag-icons/css/flag-icons.min.css";
-
+import { useSession, signOut } from "next-auth/react";
+import { useState } from "react";
 // Components
 import Link from "next/link";
 
-import StyledTitle from "../ui/components/typography/StyledTitle";
+import StyledTitle from "@/app/ui/components/typography/StyledTitle";
+import GameAlert from "@/app/ui/components/modals/GameAlert";
+
 import ProfileOptionRow from "./components/ProfileOptionRow";
+import { ProfileAvatarSelect } from "./components/ProfileAvatarSelect";
 
 // Icons
 import DriveFileRenameOutlineRoundedIcon from "@mui/icons-material/DriveFileRenameOutlineRounded";
@@ -16,65 +22,121 @@ import SupervisedUserCircleRoundedIcon from "@mui/icons-material/SupervisedUserC
 import StyledButton from "../ui/components/typography/StyledButton";
 
 export default function ProfilePage() {
+    const { data: session } = useSession();
+
+    const [showProfileAvatarSelect, setShowProfileAvatarSelect] =
+        useState(false);
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+
     return (
-        <section className="space-y-xl">
-            <header className="flex flex-wrap items-center px-md py-xl pb-md lg:justify-center lg:bg-secondary lg:py-md">
-                <figure className="relative mr-md aspect-square w-24 lg:w-28">
-                    {/* <img src="/images/avatar.jpg" alt="Avatar" className="w-24 h-24 rounded-full" /> */}
-                    <span className="block aspect-square w-full rounded-full bg-dark-2"></span>
-                    <button className="absolute bottom-0 left-2 aspect-square rounded-full bg-gray-2 p-xs text-sm">
-                        <DriveFileRenameOutlineRoundedIcon className="text-t-secondary" />
-                    </button>
-                </figure>
-                <div>
-                    <StyledTitle
-                        fontFamily="bungee"
-                        extraClasses="flex items-center !mb-sm"
-                    >
-                        John Doe
-                        <span className="fi fi-ps ml-md text-xl"></span>
-                    </StyledTitle>
-                    <StyledTitle variant="h3" extraClasses="!mb-none">
-                        @Nickname
-                    </StyledTitle>
-                </div>
-            </header>
+        <>
+            <section className="space-y-xl">
+                <header className="flex flex-col flex-wrap items-center gap-md px-md py-xl pb-md lg:bg-secondary lg:py-md">
+                    <figure className="relative mr-md aspect-square w-24 lg:w-28">
+                        {session?.data ? (
+                            <img
+                                src={`${process.env.NEXT_PUBLIC_API_URL}/assets/avatars/${session?.data.fkUserAvatarImgId}`}
+                                alt="Avatar"
+                                className="aspect-square w-full rounded-full"
+                                width="112"
+                                height="112"
+                            />
+                        ) : (
+                            <span className="block aspect-square w-full rounded-full bg-dark-1"></span>
+                        )}
+                        <button
+                            className="absolute bottom-0 left-2 aspect-square rounded-full bg-gray-2 p-xs text-sm"
+                            onClick={() => setShowProfileAvatarSelect(true)}
+                        >
+                            <DriveFileRenameOutlineRoundedIcon className="text-t-secondary" />
+                        </button>
+                    </figure>
+                    <div>
+                        <StyledTitle
+                            fontFamily="bungee"
+                            extraClasses="flex items-center !mb-sm"
+                        >
+                            {session?.data.nickname}
+                            <span className="fi fi-ps ml-md text-xl"></span>
+                        </StyledTitle>
+                    </div>
+                </header>
 
-            <section>
-                <nav className="mx-auto w-full space-y-lg px-md lg:max-w-96 lg:px-none">
-                    <ProfileOptionRow href="/profile">
-                        <AccountCircleRoundedIcon /> Account Details
-                    </ProfileOptionRow>
-                    <ProfileOptionRow href="/profile">
-                        <HistoryRoundedIcon />
-                        Game History
-                    </ProfileOptionRow>
-                    <ProfileOptionRow href="/profile">
-                        <ShowChartRoundedIcon /> User statistics
-                    </ProfileOptionRow>
-                    <ProfileOptionRow href="/profile">
-                        <SupervisedUserCircleRoundedIcon /> My friends
-                    </ProfileOptionRow>
-                    <ProfileOptionRow href="/profile">
-                        <DriveFileRenameOutlineRoundedIcon /> Customize
-                    </ProfileOptionRow>
-                </nav>
+                <section>
+                    <nav className="mx-auto w-full space-y-lg px-md lg:max-w-96 lg:px-none">
+                        <ProfileOptionRow href="/profile">
+                            <AccountCircleRoundedIcon /> Account Details
+                        </ProfileOptionRow>
+                        <ProfileOptionRow href="/profile">
+                            <HistoryRoundedIcon />
+                            Game History
+                        </ProfileOptionRow>
+                        <ProfileOptionRow href="/profile">
+                            <ShowChartRoundedIcon /> User statistics
+                        </ProfileOptionRow>
+                        <ProfileOptionRow href="/profile">
+                            <SupervisedUserCircleRoundedIcon /> My friends
+                        </ProfileOptionRow>
+                        <ProfileOptionRow href="/profile">
+                            <DriveFileRenameOutlineRoundedIcon /> Customize
+                        </ProfileOptionRow>
+                    </nav>
+                </section>
+
+                <section>
+                    <nav className="mx-auto w-full space-y-md px-md lg:max-w-96 lg:px-none">
+                        <StyledButton
+                            extraClasses="w-full"
+                            style="outlined"
+                            onClick={() => signOut()}
+                        >
+                            Log out
+                        </StyledButton>
+
+                        <Link
+                            href="/profile"
+                            className="block w-full rounded-base border-2 border-transparent px-md py-sm text-center text-error transition-colors hover:border-error"
+                            onClick={() => setShowConfirmDialog(true)}
+                        >
+                            Delete account
+                        </Link>
+                    </nav>
+                </section>
             </section>
 
-            <section>
-                <nav className="mx-auto w-full space-y-md px-md lg:max-w-96 lg:px-none">
-                    <StyledButton extraClasses="w-full" style="outlined">
-                        Log out
-                    </StyledButton>
+            {showProfileAvatarSelect && session?.data ? (
+                <ProfileAvatarSelect
+                    onClose={() => setShowProfileAvatarSelect(false)}
+                    currentAvatarId={session?.data.fkUserAvatarImgId}
+                />
+            ) : (
+                <></>
+            )}
 
-                    <Link
-                        href="/profile"
-                        className="block w-full rounded-base border-2 border-transparent px-md py-sm text-center text-error transition-colors hover:border-error"
-                    >
-                        Delete account
-                    </Link>
-                </nav>
-            </section>
-        </section>
+            {showConfirmDialog ? (
+                <GameAlert
+                    close={() => setShowConfirmDialog(false)}
+                    size="large"
+                >
+                    <StyledTitle variant="h4" extraClasses="text-center !mb-lg">
+                        Are you sure you want to delete your account?
+                    </StyledTitle>
+                    <p className="text-slate-500 text-center">
+                        This action is irreversible and all your data will be
+                        lost.
+                    </p>
+                    <div className="mt-xl flex justify-center gap-md">
+                        <StyledButton
+                            onClick={() => setShowConfirmDialog(false)}
+                        >
+                            Cancel
+                        </StyledButton>
+                        <StyledButton style="outlined">Delete</StyledButton>
+                    </div>
+                </GameAlert>
+            ) : (
+                <></>
+            )}
+        </>
     );
 }
