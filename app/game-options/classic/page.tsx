58387c64633f6208c9daab39d7c1f3f68da24c34
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-09-16 21:02:43 -0500

    Dropdown button con mapeo y cambio de estilos

--- app/game-options/classic/page.tsx
+++ app/game-options/classic/page.tsx
@@ -5,11 +5,13 @@ import Image from "next/image";
 import StyledTitle from "@/app/ui/components/typography/StyledTitle";
 import StyledButton from "@/app/ui/components/typography/StyledButton";
 import BetOption from "@/app/game-options/components/bet-options";
-import DropdownButton from "@/app/game-options/components/drop-down";
+import Dropdown from "@/app/game-options/components/drop-down";
 
 // Importing icons
 import Clock from "@/app/ui/icons/clock.svg";
-import ArrowDown from "@/app/ui/icons/down-arrow.svg";
+import ChessTile from "@/app/ui/icons/chess-tile.svg";
+import Dice from "@/app/ui/icons/dice.svg";
+import Link from "@/app/ui/icons/link-shared.svg";
 
 export default function ClassicOptionPage() {
     return (
@@ -18,43 +20,65 @@ export default function ClassicOptionPage() {
                 Game Options
             </StyledTitle>
 
-            <DropdownButton />
+            <Dropdown dropStyles="outlined" />
 
             <StyledTitle variant="h2" extraClasses="text-center">
                 Your bet
             </StyledTitle>
-
-            <fieldset className="flex justify-center">
-                {BetOption("0")}
-                {BetOption("20")}
-                {BetOption("60")}
-                {BetOption("100")}
-            </fieldset>
-            <fieldset className="flex justify-center">
-                {BetOption("220")}
-                {BetOption("580")}
-                {BetOption("1000")}
-            </fieldset>
-
+            <div>
+                <fieldset className="flex justify-center pb-lg">
+                    {BetOption("0")}
+                    {BetOption("20")}
+                    {BetOption("60")}
+                    {BetOption("100")}
+                </fieldset>
+                <fieldset className="flex justify-center">
+                    {BetOption("220")}
+                    {BetOption("580")}
+                    {BetOption("1000")}
+                </fieldset>
+            </div>
             <StyledTitle variant="h2" extraClasses="text-center">
                 Against Who?
             </StyledTitle>
             <div>
                 <div className="flex items-center justify-center py-md">
-                    <StyledButton extraClasses={"w-full"}>
+                    <StyledButton extraClasses={"w-full flex justify-center"}>
+                        <Image
+                            src={Dice}
+                            alt=""
+                            width={20}
+                            height={20}
+                            className="h-auto w-auto pr-sm"
+                        />
                         Play vs. Random
                     </StyledButton>
                 </div>
                 <div className="flex items-center justify-center">
-                    <StyledButton extraClasses={"w-full"}>
+                    <StyledButton extraClasses={"w-full flex justify-center"}>
+                        <Image
+                            src={Link}
+                            alt=""
+                            width={20}
+                            height={20}
+                            className="h-auto w-auto pr-sm"
+                        />
                         Copy Game’s Link
                     </StyledButton>
                 </div>
                 <div className="flex items-center justify-center py-md">
-                    <StyledButton extraClasses={"w-full"}>
+                    <StyledButton extraClasses={"w-full flex justify-center"}>
+                        <Image
+                            src={ChessTile}
+                            alt=""
+                            width={20}
+                            height={20}
+                            className="h-auto w-auto pr-sm"
+                        />
                         Play vs. a Friend
                     </StyledButton>
                 </div>
+
                 <div className="flex items-center justify-center p-md">
                     <StyledButton style="outlined" extraClasses="!px-[32px]">
                         Cancel
