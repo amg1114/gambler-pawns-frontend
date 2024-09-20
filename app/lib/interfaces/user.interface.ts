1ccf1f3e60db962bd84ab51450965cdaba153e71
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-19 21:01:30 -0500

    refactor: Update User interface to match the new response

--- app/lib/interfaces/user.interface.ts
+++ app/lib/interfaces/user.interface.ts
@@ -2,18 +2,22 @@ export interface User {
     userId:                 number;
     nickname:               string;
     email:                  string;
+    dateOfBirth:            null;
     countryCode:            string;
-    about:                  string;
-    fkUserAvatarImgId:      number;
+    aboutText:              string;
     eloRapid:               number;
     eloBlitz:               number;
     eloBullet:              number;
     eloArcade:              number;
     currentCoins:           number;
-    acumulatedAlltimeCoins: number;
+    acumulatedAllTimeCoins: number;
     nPuzzlesSolved:         number;
     streakDays:             number;
     isDeleted:              boolean;
-    iat:                    number;
-    exp:                    number;
+    userAvatarImg:          UserAvatarImg;
+}
+
+export interface UserAvatarImg {
+    userAvatarImgId: number;
+    fileName:        string;
 }
