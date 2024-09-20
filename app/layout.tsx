c29b3c551911ca6516957f7bba91ee65920c02a6
RamSterB <joseluisramosa337@gmail.com>
2024-09-19 19:29:06 -0500

    style: main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
                             {children}
                         </main>
                     </div>
-                    
                 </Providers>
             </body>
         </html >
