56549f4babd2cece016001543a5b3dc67d003494
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-22 21:32:11 -0500

    feat: add userProfile page

---
+++ app/ui/components/user/UserAvatar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+
+export default function UserAvatar({
+    filename,
+}: {
+    filename: string | null | undefined;
+}) {
+    return (
+        <>
+            {filename ? (
+                <Image
+                    src={`${process.env.NEXT_PUBLIC_AVATAR_URL}/${filename}`}
+                    alt="Avatar"
+                    className="aspect-square w-full rounded-full"
+                    width="112"
+                    height="112"
+                />
+            ) : (
+                <span className="block aspect-square w-full rounded-full bg-dark-1"></span>
+            )}
+        </>
+    );
+}
