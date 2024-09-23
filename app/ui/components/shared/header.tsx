348e78d8b91a0481dc61665f80e5a4c3163beaaa
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-22 20:42:05 -0500

    fix: remove header unnecessary padding, svg properties

--- app/ui/components/shared/header.tsx
+++ app/ui/components/shared/header.tsx
@@ -70,7 +70,7 @@ export default function Header() {
 
     return (
         <>
-            <header className="fixed top-0 z-50 flex w-full items-center justify-between border-b-2 border-primary bg-dark-1 p-md">
+            <header className="sticky top-0 z-50 flex w-full items-center justify-between border-b-2 border-primary bg-dark-1 p-md">
                 <div className="flex w-1/3 bg-dark-1 px-sm py-sm max-[370px]:w-1/4">
                     <div className="flex items-center justify-start rtl:justify-end">
                         <button
@@ -87,7 +87,7 @@ export default function Header() {
                             >
                                 <path
                                     clipRule="evenodd"
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
                                 ></path>
                             </svg>
