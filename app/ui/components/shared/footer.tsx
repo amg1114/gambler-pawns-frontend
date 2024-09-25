c44fa3418ed60466d48fa4fe2d1a7e3aab3d9ef1
JuanIdarraga <jupaida@gmail.com>
2024-09-24 20:31:44 -0500

    format

--- app/ui/components/shared/footer.tsx
+++ app/ui/components/shared/footer.tsx
@@ -1,30 +1,34 @@
-import StyledTitle from "../typography/StyledTitle"
-import githubIcon from "../../icons/githubIcon.svg"
-import StyledLink from "../typography/StyledLink"
-import Image from "next/image"
-import StyledParagraph from "../typography/StyledParagraph"
+import StyledTitle from "../typography/StyledTitle";
+import githubIcon from "../../icons/githubIcon.svg";
+import StyledLink from "../typography/StyledLink";
+import Image from "next/image";
+import StyledParagraph from "../typography/StyledParagraph";
 
 export default function Footer(): JSX.Element {
-    
-    return(
-    <footer className="w-full text-light mx-auto mt-2xl">
-          <div className="flex flex-col items-center mb-none w-full">
-              <StyledLink href="/about" extraClasses="!p-xs hover:text-primary transition-colors mx-[50%] w-[120px]">
-                About Us
-              </StyledLink>
-              <a 
-                href="https://github.com/amg1114/gambler-pawns-frontend" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="flex items-center "
-              >
-                <Image src={githubIcon} alt="" className="mr-sm size-lg " />
-                <StyledParagraph extraClasses="mb-none">GitHub Repository</StyledParagraph>
-              </a>
-          </div>
-          <div className="mt-md  border-t  text-center text-sm text-dark-2">
-            © {new Date().getFullYear()} Gambler Pawns. All rights reserved.
-          </div>
-      </footer>)
-
+  return (
+    <footer className="mx-auto mt-2xl w-full text-light">
+      <div className="mb-none flex w-full flex-col items-center">
+        <StyledLink
+          href="/about"
+          extraClasses="!p-xs hover:text-primary transition-colors mx-[50%] w-[120px]"
+        >
+          About Us
+        </StyledLink>
+        <a
+          href="https://github.com/amg1114/gambler-pawns-frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center"
+        >
+          <Image src={githubIcon} alt="" className="mr-sm size-lg" />
+          <StyledParagraph extraClasses="mb-none">
+            GitHub Repository
+          </StyledParagraph>
+        </a>
+      </div>
+      <div className="mt-md border-t text-center text-sm text-dark-2">
+        © {new Date().getFullYear()} Gambler Pawns. All rights reserved.
+      </div>
+    </footer>
+  );
 }
