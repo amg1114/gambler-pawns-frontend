50b8743e87c4d266453d16167e963cb6ee65b825
Alejandro Moreno <alejandromogil@gmail.com>
2024-09-24 20:11:41 -0500

    fix: WebSocket connection to support secure connections

--- app/game/_hooks/useGameConnection.ts
+++ app/game/_hooks/useGameConnection.ts
@@ -35,7 +35,9 @@ export function useGameConnection({
 
   useLayoutEffect(() => {
     // Conectar al servidor WebSocket
-    const newSocket = io(process.env.NEXT_PUBLIC_WS_URL);
+    const newSocket = io(process.env.NEXT_PUBLIC_WS_URL, {
+      secure: process.env.NEXT_PUBLIC_WS_SSL === "true",
+    });
     setSocket(newSocket);
 
     if (gameId !== undefined) {
