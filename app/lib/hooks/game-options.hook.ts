a8f58978e4b06870dd3bb9fc7c95cab08dfab8c0
Alejandro Moreno <alejandromogil@gmail.com>
2024-10-13 17:35:22 -0500

    feature: create `getGameOptions` & `clearGameOptions` hooks

--- app/lib/hooks/game-options.hook.ts
+++ app/lib/hooks/game-options.hook.ts
@@ -5,7 +5,6 @@ type GameOptions = {
   timeInSeconds?: number;
 };
 
-type GameOptionsKey = keyof GameOptions;
 export const setGameOptions = (option: GameOptions) => {
   try {
     let gameOptions = JSON.parse(localStorage.getItem("gameOptions") || "{}");
@@ -18,5 +17,22 @@ export const setGameOptions = (option: GameOptions) => {
     localStorage.setItem("gameOptions", JSON.stringify(gameOptions));
   } catch (error) {
     console.error("Error setting game options:", error);
+  }
+};
+
+export const getGameOptions = (): GameOptions => {
+  try {
+    return JSON.parse(localStorage.getItem("gameOptions") || "{}");
+  } catch (error) {
+    console.error("Error getting game options:", error);
+    return {};
+  }
+};
+
+export const clearGameOptions = () => {
+  try {
+    localStorage.removeItem("gameOptions");
+  } catch (error) {
+    console.error("Error clearing game options:", error);
   }
 };
