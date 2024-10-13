b8c789a7056b10dac34df8130f7f07182e65d730
Alejandro Moreno <alejandromogil@gmail.com>
2024-10-13 17:35:39 -0500

    refactor: set bet config on click event

--- app/game-options/components/bet-options.tsx
+++ app/game-options/components/bet-options.tsx
@@ -1,6 +1,10 @@
 import StyledParagraph from "@/app/ui/components/typography/StyledParagraph";
-
+import { setGameOptions } from "@/app/lib/hooks/game-options.hook";
 export default function BetOption(id: string) {
+  const handleBetSelection = (id: string) => {
+    setGameOptions({ bet: +id });
+  };
+
   return (
     <div className="px-sm">
       <label htmlFor={"bet" + id} className="cursor-pointer">
@@ -10,6 +14,7 @@ export default function BetOption(id: string) {
           type="radio"
           name="status"
           defaultChecked={id === "0"}
+          onClick={() => handleBetSelection(id)}
         />
         <div className="h-6 rounded-base bg-green-board px-md text-light ring-2 ring-transparent peer-checked/bet:bg-green-hover peer-checked/bet:ring-accent peer-checked:ring-offset-2">
           <div className="flex justify-center">
