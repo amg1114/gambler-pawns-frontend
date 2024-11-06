881838ed77b2939d15f48cd306df3f5f085205e3
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-11-05 22:16:13 -0500

    Estilo para mostrar partidas terminado

--- app/game-history/page.tsx
+++ app/game-history/page.tsx
@@ -1,15 +1,19 @@
 "use client";
-import { azeret_mono } from "@/app/ui/fonts";
+import { azeret_mono, nunito } from "@/app/ui/fonts";
+import Image from "next/image";
 
 // Importing components
 import StyledTitle from "@/app/ui/components/typography/StyledTitle";
 import Dropdown from "@/app/game-options/components/drop-down";
+import StyledButton from "@/app/ui/components/typography/StyledButton";
+import Aguacate from "@/app/ui/icons/aguacate.png";
 
 // =========  Zod Schema =======
 // TODO: refactor this in a separate file
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { URL } from "next/dist/compiled/@edge-runtime/primitives/url";
 
 const gameOptionsSchema = z.object({
   // TODO: validate if is in enum, and refactor selection options as a
@@ -84,12 +88,24 @@ export default function GameHistoryPage() {
         >
           <Dropdown.ResultType></Dropdown.ResultType>
         </Dropdown>
-        <div>
-          <span>
-            <img src="/images/empty-state.svg" alt="empty state" />
-            Pepito123
-          </span>
-        </div>
+      </div>
+      <div className="flex">
+        <Image
+          src={Aguacate}
+          alt=""
+          width={52}
+          height={52}
+          className="h-14 w-14"
+        />
+        <ul className="pl-md">
+          <li className={`${azeret_mono.className} font-bold`}>
+            Nombre-usuario (elo)
+          </li>
+          <li className={`${nunito.className} pb-md font-light`}>
+            modo de juego - fecha
+          </li>
+          <StyledButton extraClasses="py-xs">Watch Again</StyledButton>
+        </ul>
       </div>
     </section>
   );
