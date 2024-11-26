b2ed5838818e695b269d181f0800295ffe7e53e3
JuanIdarraga <jupaida@gmail.com>
2024-11-25 23:49:31 -0500

    format-imports

--- app/ui/components/GlobalNotificationWrapper.tsx
+++ app/ui/components/GlobalNotificationWrapper.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useWebSocketConnection } from "@/app/lib/contexts/WebSocketContext";
-import Link from "next/link";
 import StyledTitle from "./typography/StyledTitle";
 import StyledButton from "./typography/StyledButton";
 import StyledParagraph from "./typography/StyledParagraph";
@@ -59,44 +58,41 @@ export default function GlobalNotificationWrapper({
   };
 
   return (
-    console.log("DATA", notification),
-    (
-      <>
-        {showNotification && (
-          <div className="lg:scale-85 fixed bottom-0 right-0 h-auto w-auto max-w-sm transform rounded-base bg-dark-2 p-md shadow-xl transition-all duration-300 ease-in-out sm:max-w-sm sm:scale-100 md:max-w-md md:scale-100 lg:max-w-auto xl:max-w-auto xl:scale-100">
-            <StyledTitle variant="h3" extraClasses="text-center">
-              {notification.title}
-            </StyledTitle>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_AVATAR_URL}/${notification.userWhoSend.userAvatarImg.fileName}`}
-              alt={`${notification.userWhoSend.nickname}'s avatar`}
-              className="mx-auto mb-md w-2xl rounded-full"
-              width={52}
-              height={52}
-            />
-            <StyledParagraph extraClasses="text-center">
-              {notification.userWhoSend.nickname} {notification.message}
-            </StyledParagraph>
-            <div className="flex justify-center">
-              <StyledButton
-                variant="primary"
-                extraClasses="text-center mx-sm"
-                onClick={handleViewClick}
-              >
-                View
-              </StyledButton>
-              <StyledButton
-                variant="primary"
-                style="outlined"
-                onClick={handleClick}
-              >
-                Close
-              </StyledButton>
-            </div>
+    <>
+      {showNotification && (
+        <div className="lg:scale-85 fixed bottom-0 right-0 h-auto w-auto max-w-sm transform rounded-base bg-dark-2 p-md shadow-xl transition-all duration-300 ease-in-out sm:max-w-sm sm:scale-100 md:max-w-md md:scale-100 lg:max-w-auto xl:max-w-auto xl:scale-100">
+          <StyledTitle variant="h3" extraClasses="text-center">
+            {notification.title}
+          </StyledTitle>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_AVATAR_URL}/${notification.userWhoSend.userAvatarImg.fileName}`}
+            alt={`${notification.userWhoSend.nickname}'s avatar`}
+            className="mx-auto mb-md w-2xl rounded-full"
+            width={52}
+            height={52}
+          />
+          <StyledParagraph extraClasses="text-center">
+            {notification.userWhoSend.nickname} {notification.message}
+          </StyledParagraph>
+          <div className="flex justify-center">
+            <StyledButton
+              variant="primary"
+              extraClasses="text-center mx-sm"
+              onClick={handleViewClick}
+            >
+              View
+            </StyledButton>
+            <StyledButton
+              variant="primary"
+              style="outlined"
+              onClick={handleClick}
+            >
+              Close
+            </StyledButton>
           </div>
-        )}
-        {children}
-      </>
-    )
+        </div>
+      )}
+      {children}
+    </>
   );
 }
