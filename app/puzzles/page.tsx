10c8868c1325769040803cd0e48c301cdb360c22
jfmonsa <juanmonsalve@protonmail.com>
2024-11-26 22:55:05 -0500

    feat: enhance useChessPuzzles hook with promotion piece handling and refactor move logic; update PuzzlePage to utilize new hook structure

--- app/puzzles/page.tsx
+++ app/puzzles/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import axios from "@/app/lib/_axios";
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { PuzzleResponse } from "../lib/interfaces/responses/puzzle-res.interface";
 import { useRouter } from "next/navigation";
 import PageLoadSpinner from "../ui/components/PageLoadSpinner";
 
 export default function Page() {
   const router = useRouter();
-  useEffect(() => {
+
+  useLayoutEffect(() => {
     axios.get<PuzzleResponse>("/puzzle/random").then((response) => {
       const puzzle = response.data.data;
 
