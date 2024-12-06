56281511504cb31420632e5879896782e22ced85
Juan Sebastian Marin Serna <sebasagosto2003@gmail.com>
2024-12-06 00:40:30 -0500

    feat: add friendship change handling and improve localStorage management in Friends components

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,22 +1,31 @@
+"use client";
+
 // Importing components
 import StyledTitle from "@/app/ui/components/typography/StyledTitle";
 import SearchUsers from "./_components/search-users";
 import MyFriends from "./_components/my-friends";
+import { useState } from "react";
 
 export default function FriendsPage() {
+  const [friendshipChangeCounter, setFriendshipChangeCounter] = useState(0);
+
+  const handleFriendshipChange = () => {
+    setFriendshipChangeCounter((prev) => prev + 1);
+  };
+
   return (
     <div className="mt-lg grid w-[850px] grid-cols-1 gap-12 lg:grid-cols-2">
       <div>
         <StyledTitle variant="h1" extraClasses="text-center">
           Search Users
         </StyledTitle>
-        <SearchUsers />
+        <SearchUsers onFriendshipChange={handleFriendshipChange} />
       </div>
       <div>
         <StyledTitle variant="h1" extraClasses="text-center">
           My Friends
         </StyledTitle>
-        <MyFriends />
+        <MyFriends onFriendshipChange={handleFriendshipChange} />
       </div>
     </div>
   );
