7aaab46a235017b400cc908495ed0e9e7ccd2517
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-08 12:42:08 -0500

    feat: add delete user functionality with confirmation dialog and error handling

---
+++ app/lib/interfaces/responses/deleteUser-res.interface.ts
@@ -0,0 +1,14 @@
+export interface DeleteUserRes {
+    status:     boolean;
+    statusCode: number;
+    path:       string;
+    data:       Data;
+    timestamp:  Date;
+}
+
+export interface Data {
+    generatedMaps: any[];
+    raw:           any[];
+    affected:      number;
+    message?:       string[];
+}
